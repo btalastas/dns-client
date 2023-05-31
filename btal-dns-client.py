e65e6f31c274190d3099ce92206e35d40fe674fe
bjornhehe <talastasbjorn@gmail.com>
2023-05-31 00:17:11 -0400

    decoded dns response

--- btal-dns-client.py
+++ btal-dns-client.py
@@ -2,6 +2,7 @@ import socket
 import sys
 import struct
 import random
+from dnslib import DNSRecord
 
 
 def create_dns_query(hostname):
@@ -13,6 +14,7 @@ def create_dns_query(hostname):
     Returns:
         bytes: DNS query message in bytes.
     """
+    print("Preparing DNS query..")
     # Header
     header_row_0 = random.randint(0, 65535)
     # header_row_0 = 1000
@@ -73,17 +75,49 @@ def send_dns_query_message(server, port, query):
     udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     try:
+        print("Contacting DNS Server..")
         udp_sock.sendto(query, (server, port))
+        print("Sending DNS query")
         udp_sock.settimeout(5)
         data = udp_sock.recv(1024)
-
-        process_dns_response(data)
+        process_dns_response(query, data)
     finally:
         udp_sock.close()
 
 
-def process_dns_response(response):
-    print(response)
+def process_dns_response(query, response):
+    """Function to print the DNS query and response
+
+    Args:
+        query (bytes): DNS query message
+        response (bytes): DNS response message
+    """
+    print("Processing DNS response..")
+
+    dns_response = DNSRecord.parse(response)
+    dns_header = dns_response.header
+    dns_question = dns_response.questions[0]
+    dns_answer = dns_response.rr
+    print(
+        "----------------------------------------------------------------------------"
+    )
+    print(
+        f"header.ID = {dns_header.id}\nheader.QR = {dns_header.qr}\n"
+        f"header.OPCODE = {dns_header.opcode}\nheader.RD = {dns_header.rd}\n"
+        f"header.QDCOUNT = {dns_header.q}"
+    )
+    print(
+        f"question.QNAME = {dns_question.qname}\nquestion.QTYPE = {dns_question.qtype}\n"
+        f"question.QCLASS = {dns_question.qclass}"
+    )
+    print(
+        f"answer.NAME = {dns_answer[0].rname}\nanswer.TYPE = {dns_answer[0].rtype}\n"
+        f"answer.CLASS = {dns_answer[0].rclass}\nanswer.TTL = {dns_answer[0].ttl}\n"
+        f"answer.RDATA = {dns_answer[0].rdata}"
+    )
+    print(
+        "----------------------------------------------------------------------------"
+    )
 
 
 # Error checking for command line arguments
